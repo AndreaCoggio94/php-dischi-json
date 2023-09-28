80af23d79d6be38515b752101145b75b1f0d6a90
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-09-28 17:47:17 +0200

    Bonus 1, first draft

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -4,6 +4,8 @@ createApp({
   data() {
     return {
       disks: [],
+      hidden: "hidden",
+      hiddenDisk: [],
     };
   },
   mounted() {
@@ -11,11 +13,21 @@ createApp({
       .get("http://localhost/php-dischi-json/backend/server.php")
       .then((response) => {
         const apiDisk = response.data.map((disk) => {
-          console.log(disk);
           const { title, genre, author, poster, year } = disk;
           return { title, genre, author, poster, year };
         });
         this.disks = apiDisk;
       });
+  },
+  methods: {
+    openDisk(index) {
+      axios
+        .get("http://localhost/php-dischi-json/backend/server.php")
+        .then((response) => {
+          console.log(response.data[index]);
+          this.hiddenDisk = response.data[index];
+        });
+      this.hidden = "absolute";
+    },
   },
 }).mount("#app");
