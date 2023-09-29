659b9322980a697cf85fc9a0925580d04247e03f
AndreaCoggio94 <andreacoggio94@gmail.com>
2023-09-29 15:34:28 +0200

    trying to get file directly from dischi.json

--- backend/server.php
+++ backend/server.php
@@ -1,10 +1,26 @@
 <?php 
 
-include __DIR__ . "/data/dischi.php" ;
+// include __DIR__ . "/data/dischi.php" ;
 
+$string = file_get_contents("../data/dischi.json");
+$disk_list = json_decode($string, true) ;
+$result = [];
+// var_dump($disk_list);
+
+if(isset($_GET["index"])) {
+    
+
+    
+    $disk = $disk_list[$_GET["index"]] ;
+    $result = $disk; 
+} else {
+    $result = $disk_list;
+}
 
 
 header('Content-Type: application/json');
-echo json_encode($dischi);
+echo json_encode($result);
+
+
 
 ?>
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -6,27 +6,31 @@ createApp({
       disks: [],
       hidden: "hidden",
       hiddenDisk: [],
+      apiUrl: "http://localhost/php-dischi-json/backend/server.php",
     };
   },
   mounted() {
-    axios
-      .get("http://localhost/php-dischi-json/backend/server.php")
-      .then((response) => {
-        const apiDisk = response.data.map((disk) => {
-          const { title, genre, author, poster, year } = disk;
-          return { title, genre, author, poster, year };
-        });
-        this.disks = apiDisk;
-      });
+    axios.get(this.apiUrl).then((response) => {
+      console.log(response);
+      // const apiDisk = response.data.map((disk) => {
+      //   const { title, genre, author, poster, year } = disk;
+      //   return { title, genre, author, poster, year };
+      // });
+      this.disks = response.data;
+    });
   },
   methods: {
     openDisk(index) {
-      axios
-        .get("http://localhost/php-dischi-json/backend/server.php")
-        .then((response) => {
-          console.log(response.data[index]);
-          this.hiddenDisk = response.data[index];
-        });
+      const diskIndex = {
+        params: {
+          index,
+        },
+      };
+
+      axios.get(this.apiUrl, diskIndex).then((response) => {
+        console.log(response);
+        // this.hiddenDisk = response.data[index];
+      });
       this.hidden = "absolute";
     },
   },
